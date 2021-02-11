47248aa83c5b093603b902d0147181f6b9e8d251
Zefir <zefirkerozovv@gmail.com>
2021-02-11 21:56:56 +0200

    Add part of object

--- JavaScript-Advanced/Objects-and-Composition/Lab/03_CityTaxes.js
+++ JavaScript-Advanced/Objects-and-Composition/Lab/03_CityTaxes.js
@@ -1,8 +1,23 @@
-function recorder(name, population, treasury){
+function cityTaxes(name, population, treasury){
     const myObj = {
         name: name,
         population: population,
         treasury: treasury,
-        taxRate: 10
+        taxRate: 10,
+        collectTaxes() {
+            this.treasury += this.population * this.taxRate;
+        },
+        applyGrowth(percentage) {
+            this.population += Math.floor(population * percentage / 100);
+        },
+        applyRecession(percentage)  {
+            this.treasury -= Math.ceil(this.treasury * percentage /100);
+        }
     }
+    return myObj
 } 
+const city = 
+  cityTaxes('Tortuga',
+  7000,
+  15000);
+console.log(city);
