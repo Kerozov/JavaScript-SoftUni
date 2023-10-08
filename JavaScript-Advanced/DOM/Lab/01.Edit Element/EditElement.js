65b2713d234271bbcbf0e29c54836e3ffa1be842
Zefir <56188831+Kerozov@users.noreply.github.com>
2023-10-08 23:10:17 +0300

    Add DOM exercise

--- JavaScript-Advanced/DOM/Lab/01.Edit Element/EditElement.js
+++ JavaScript-Advanced/DOM/Lab/01.Edit Element/EditElement.js
@@ -1,5 +1,8 @@
 function editElement(ref, match, replacer) {
   const matcher = new RegExp(match, 'g');
-  const result = ref.textContent.replace(matcher, replacer);
+  const result = ref.textContent.replace(matcher.substring(1, match.length - 1), replacer);
   ref.textContent = result;
 }
+const submit = document.getElementById("submitButton")
+submit.addEventListener("click", editElement(document.getElementById("e1"),'%insert you text here%',document.getElementById("inputText")))
+
