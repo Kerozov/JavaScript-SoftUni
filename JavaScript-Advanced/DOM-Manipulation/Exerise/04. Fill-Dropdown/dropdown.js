826d97ea3f57a1bf86dc2ad79e3ed64f718b1f5a
Zefir <56188831+Kerozov@users.noreply.github.com>
2023-10-16 01:56:45 +0300

    Add more exercise

--- JavaScript-Advanced/DOM-Manipulation/Exerise/04. Fill-Dropdown/dropdown.js
+++ JavaScript-Advanced/DOM-Manipulation/Exerise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,11 @@
 function addItem() {
-    console.log('TODO:...');
+    let name = document.getElementById('newItemText');
+    let value = document.getElementById('newItemValue');
+    const option = document.createElement('option');
+    option.textContent = name.value;
+    option.value = value.value;
+    const dropdown = document.getElementById('menu')
+    dropdown.appendChild(option);
+    name.value = '';
+    value.value = '';
 }
