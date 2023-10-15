5ce659713a2a98eb3dedc7c81ec103cc16078890
Zefir <56188831+Kerozov@users.noreply.github.com>
2023-10-16 02:32:18 +0300

    Add 05_EncodeAndDecode

--- JavaScript-Advanced/DOM-Manipulation/Exerise/05. Encode-and-Decode-Messages/app.js
+++ JavaScript-Advanced/DOM-Manipulation/Exerise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,27 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let buttons = [...document.querySelectorAll('button')];
+    buttons.forEach(e => e.addEventListener('click', onClick) )
+
+    function onClick(event){
+    let textArea = [...document.querySelectorAll('textarea')];
+
+        if (event.target.textContent == 'Encode and send it') {
+           let text = [...textArea[0].value.split('')];
+          text =  text.map(char => { 
+                const asciiCode = char.charCodeAt(0);
+                const newAsciiCode = asciiCode + 1;
+                return String.fromCharCode(newAsciiCode);
+            });
+            textArea[1].value = text.join('');
+            textArea[0].value = '';
+        } else {
+            let text =   [...textArea[1].value.split('')];
+            text =  text.map(char => { 
+                const asciiCode = char.charCodeAt(0);
+                const newAsciiCode = asciiCode - 1;
+                return String.fromCharCode(newAsciiCode);
+            });
+            textArea[1].value = text.join('');
+        }
+    }
 }
